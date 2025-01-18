8ce23c7eb878d0c319c5b4450333e8e18cb8a923
jduque1989 <67889006+jduque1989@users.noreply.github.com>
2025-01-18 09:40:20 -0500

    Adding wrap to .nvim config, Adding insert book to obsidian
    Create a Obsidian setup to wrap .md files, to optimize note taking

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -20,39 +20,39 @@ return {
     templates = {
       folder = '/Users/juandavidduquea/Library/Mobile Documents/iCloud~md~obsidian/Documents/DIGITAL BRAIN/Templates',
       book_review = [[
----
-Title: 
-Author: 
-Tags: 
-  - books
-  - abstract
-Status: 
-Rating: 
-Published: 
-ISBN: 
-Read_Start: 
-Read_End: 
-Categories: 
-  - 
-Keywords: 
-  - 
-URL: 
-Summary: 
----
+      ---
+      Title:
+      Author:
+      Tags:
+        - books
+        - abstract
+      Status:
+      Rating:
+      Published:
+      ISBN:
+      Read_Start:
+      Read_End:
+      Categories:
+        -
+      Keywords:
+        -
+      URL:
+      Summary:
+      ---
 
-# {{Title}}
+      # {{Title}}
 
-**Author:** {{Author}}
+      **Author:** {{Author}}
 
-## Key Takeaways
-- 
+      ## Key Takeaways
+      -
 
-## Insights & Applications
-- 
+      ## Insights & Applications
+      -
 
-## Favorite Quotes
-- 
-]],
+      ## Favorite Quotes
+      -
+      ]],
     },
   },
   config = function(_, opts)
@@ -81,4 +81,38 @@ Summary:
       end
     end, {})
   end,
+
+  -- Command to create a new meeting minutes note using the template in the 'templates' folder
+  vim.api.nvim_create_user_command('NewRockStart', function()
+    -- Get the path to the template
+    local template_path = '/Users/juandavidduquea/Library/Mobile Documents/iCloud~md~obsidian/Documents/DIGITAL BRAIN/templates/NewMeetingRockStart.template.md'
+    local vault_path = '/Users/juandavidduquea/Library/Mobile Documents/iCloud~md~obsidian/Documents/DIGITAL BRAIN'
+    local target_folder = vault_path .. '/Ciclos del Exito/RockStart'
+
+    -- Ensure the target folder exists
+    vim.fn.mkdir(target_folder, 'p')
+
+    -- Read the template content
+    local template_file = io.open(template_path, 'r')
+    if not template_file then
+      print('Template file not found: ' .. template_path)
+      return
+    end
+    local template_content = template_file:read '*all'
+    template_file:close()
+
+    -- Create a new file with current timestamp
+    local os_date = os.date '!%Y-%m-%d'
+    local new_note_path = target_folder .. '/Meeting_' .. os_date .. '.md'
+
+    -- Write the template content to the new file
+    local new_file = io.open(new_note_path, 'w')
+    if new_file then
+      new_file:write(template_content)
+      new_file:close()
+      print('Meeting note created at: ' .. new_note_path)
+    else
+      print('Failed to create note at: ' .. new_note_path)
+    end
+  end, {}),
 }
