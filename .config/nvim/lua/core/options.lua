d870aef6e34dee656090fa24677d906354540aa0
jduque1989 <67889006+jduque1989@users.noreply.github.com>
2025-01-18 09:40:20 -0500

    Adding wrap to .nvim config, Adding insert book to obsidian
    Create a Obsidian setup to wrap .md files, to optimize note taking

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -42,3 +42,6 @@ vim.opt.iskeyword:append '-' -- Hyphenated words recognized by searches (default
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- Separate Vim plugins from Neovim in case Vim still in use (default: includes this path if Vim is installed)
 vim.opt.conceallevel = 1
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.linebreak = true -- Avoid breaking lines in the middle of words
+vim.opt.breakindent = true -- Maintain indentation for wrapped lines
