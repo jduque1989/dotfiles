d3b69aa1118dacaa219b784c6fcdff866bb2c5d7
jduque1989 <67889006+jduque1989@users.noreply.github.com>
2025-06-12 23:49:19 -0500

    update Avante set up

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -17,49 +17,6 @@ return {
       },
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   build = 'make',
-  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --   dependencies = {
-  --     'stevearc/dressing.nvim',
-  --     'nvim-lua/plenary.nvim',
-  --     'MunifTanjim/nui.nvim',
-  --     --- The below dependencies are optional,
-  --     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-  --     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-  --     'zbirenbaum/copilot.lua',
-  --     'MunifTanjim/nui.nvim', -- for providers='copilot'
-  --     {
-  --       -- support for image pasting
-  --       'HakonHarnes/img-clip.nvim',
-  --       event = 'VeryLazy',
-  --       opts = {
-  --         -- recommended settings
-  --         default = {
-  --           embed_image_as_base64 = false,
-  --           prompt_for_file_name = false,
-  --           drag_and_drop = {
-  --             insert_mode = true,
-  --           },
-  --           -- required for Windows users
-  --           use_absolute_path = true,
-  --         },
-  --       },
-  --     },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       'MeanderingProgrammer/render-markdown.nvim',
-  --       opts = {
-  --         file_types = { 'markdown', 'Avante' },
-  --       },
-  --       ft = { 'markdown', 'Avante' },
-  --     },
-  --   },
-  --   {
-  --     'tweekmonster/startuptime.vim',
-  --     cmd = 'StartupTime',
-  --   },
-  -- }
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
@@ -68,12 +25,12 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'echasnovski/mini.pick',         -- for file_selector provider mini.pick
     'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    'hrsh7th/nvim-cmp',              -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua',              -- for file_selector provider fzf
+    'nvim-tree/nvim-web-devicons',   -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua',        -- for providers='copilot'
     {
       -- support for image pasting
       'HakonHarnes/img-clip.nvim',
@@ -101,3 +58,6 @@ return {
     },
   },
 }
+-- Configuration for Avante plugin
+-- This plugin integrates with OpenAI's API for enhanced functionality.
+-- Adjust the settings as needed for your specific use case.
